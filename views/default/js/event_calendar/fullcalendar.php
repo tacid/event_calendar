fc25ab81d8c717f5bfb85acaedbe3e117e0d5960
Paul <tacid@tacid.kiev.ua>
2013-01-29 16:49:01 +0200

    small month name fix

--- views/default/js/event_calendar/fullcalendar.php
+++ views/default/js/event_calendar/fullcalendar.php
@@ -63,8 +63,8 @@ var defaults = {
 	// locale
 	isRTL: false,
 	firstDay: 0,
-	monthNames: ['January','February','March','April','May','June','July','August','September','Октябрь','November','December'],
-	monthNamesShort: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Окт','Nov','Dec'],
+	monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+	monthNamesShort: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
 	dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
 	dayNamesShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
 	buttonText: {
